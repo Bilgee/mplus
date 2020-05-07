f593381269af834add4c30d5cd12150eae0d8050
fleshuu <khuslen0915@gmail.com>
2020-05-07 18:34:44 +0800

    Dev (#4)
    * lowercase
    
    * changed_output
    
    * changed_output
    
    * fix_bug
    
    * removed double match when Ner word is also tfidf keyword
    
    * fixed unnecessary try except blocks
    
    Co-authored-by: beegi22 <bilguunbayar22@gmail.com>

--- application/ad_topic_model.py
+++ application/ad_topic_model.py
@@ -1,6 +1,5 @@
 import fnmatch
 import json
-import gensim
 import nltk
 from gensim import corpora
 from nltk.corpus import wordnet
@@ -12,80 +11,83 @@ nltk.download('wordnet')
 wordnet_lemmatizer = WordNetLemmatizer()
 
 
+def topics_temp(category, score, total, index):
+    temp = {'category': category, 'score': round((score / total), 4), 'index': index}
+    return temp
+
+
+def topic_score(page, tdictionary, dictionary):
+    total = 0
+    t = {}  # topic buriin niit score
+    for word in page:
+        w = dictionary[word[0]]
+        w = wordnet_lemmatizer.lemmatize(w)
+        syns = wordnet.synsets(w)
+        dd = set()
+        if syns:
+            syn = syns[0]
+            if fnmatch.fnmatch(syn.name(), "*.n.*"):
+                for s in syn.lemmas():
+                    dd.add(s.name())
+        else:
+            dd.add(w)
+        s = {}
+        for ug in dd:
+            if tdictionary.get(ug):
+                temp2 = tdictionary.get(ug)
+            else:
+                continue
+            for q in temp2:
+                j = 1
+                i = 0
+                for p in range(word[1]):
+                    i += q['score'] * j
+                    j *= 0.9
+                if s.get(q['index']):
+                    if i > s[q['index']][0]:
+                        s[q['index']][0] = i
+                    if q['score'] > s[q['index']][1]:
+                        s[q['index']][1] = q['score']
+                else:
+                    s[q['index']] = []
+                    s[q['index']].append(i)
+                    s[q['index']].append(q['score'])
+        for wo in s:
+            total += s[wo][0]
+            if t.get(wo):
+                t[wo][0] += s[wo][0]
+                t[wo][1] += s[wo][1]
+            else:
+                t[wo] = []
+                t[wo].append(s[wo][0])
+                t[wo].append(s[wo][1])
+    return t, total
+
+
+def score(temp, tlist, total):
+    for t in tlist:
+        temp['topics'].append(topics_temp(t[2], t[0], total, t[1]))
+    return temp
+
+
 class AdTopicModel:
     def __init__(self):
         """init class object
 
         Arguments:
             model -- [topics]
-            Tdictionary  -- [topic words dictionary] 
+            Tdictionary  -- [topic words dictionary]
         """
         with open("application/Newtopic.txt") as json_file:
             self.model = json.load(json_file)
         with open("application/Tdictionary.txt") as json_file:
             self.Tdictionary = json.load(json_file)
 
-    def topics_temp(self, category, score, total, index):
-        temp = {'category': category, 'score': round((score / total), 4), 'index': index}
-        return temp
-
-    def topic_score(self, page, tdictionary, dictionary):
-        total = 0
-        t = {}  # topic buriin niit score
-        for word in page:
-            w = dictionary[word[0]]
-            w = wordnet_lemmatizer.lemmatize(w)
-            syns = wordnet.synsets(w)
-            dd = set()
-            try:
-                syn = syns[0]
-                if fnmatch.fnmatch(syn.name(), "*.n.*"):
-                    for s in syn.lemmas():
-                        dd.add(s.name())
-            except:
-                dd.add(w)
-            s = {}
-            for ug in dd:
-                try:
-                    temp2 = tdictionary[ug]
-                except KeyError:
-                    continue
-                for q in temp2:
-                    j = 1
-                    i = 0
-                    for p in range(word[1]):
-                        i += q['score'] * j
-                        j *= 0.9
-                    try:
-                        if i > s[q['index']][0]:
-                            s[q['index']][0] = i
-                        if q['score'] > s[q['index']][1]:
-                            s[q['index']][1] = q['score']
-                    except KeyError:
-                        s[q['index']] = []
-                        s[q['index']].append(i)
-                        s[q['index']].append(q['score'])
-            for wo in s:
-                total += s[wo][0]
-                try:
-                    t[wo][0] += s[wo][0]
-                    t[wo][1] += s[wo][1]
-                except KeyError:
-                    t[wo] = []
-                    t[wo].append(s[wo][0])
-                    t[wo].append(s[wo][1])
-        return t, total
-
-    def score(self, temp, tlist, total):
-        for t in tlist:
-            temp['topics'].append(self.topics_temp(t[2], t[0], total, t[1]))
-        return temp
-
     def topic_predict(self, bow_corpus, dictionary):
-        predicted = [] 
+        predicted = []
         for page in bow_corpus:
             temp = {'topics': []}
-            t, total = self.topic_score(page, self.Tdictionary, dictionary)
+            t, total = topic_score(page, self.Tdictionary, dictionary)
             tlist = []
             for q in t:
                 temp2 = self.model['topics'][q]['category']  # q ni topiciin index
@@ -101,7 +103,7 @@ class AdTopicModel:
                 temp['topics'].append(temp2)
                 predicted.append(temp)
                 continue
-            temp = self.score(temp, tlist, total)
+            temp = score(temp, tlist, total)
             predicted.append(temp)
         return predicted
 
